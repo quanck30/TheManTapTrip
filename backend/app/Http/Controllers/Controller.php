7420c6c6ba10e457227b885cc35cd90a1bc79943
take <kta4486@gmail.com>
2026-06-05 13:51:46 +0900

    質問API修正版

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -4,6 +4,5 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    // 返し方
-    // return response()->json($data, 200, $headers);
+
 }
--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -16,12 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'display_name' => 'Test User',
-
-        // ]);
         $this->call([
             QuestionsTableSeeder::class,
             QueryItemsTableSeeder::class
