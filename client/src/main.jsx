caed38a8c215a46bbe62d9f6891c2e5a752d701f
Jin-Nakamyouzu <3n3.nazu3.Naka@gmail.com>
2026-06-05 11:57:28 +0900

    Googleログイン機能のためのカスタムフックとテストページを追加 取り敢えずのコミットで後で取り消し予定

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { TestPage } from "./pages/test.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
+            <TestPage />
+        </GoogleOAuthProvider>
+    </StrictMode>,
+);
